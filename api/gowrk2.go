335f23502f53b5f1d8f8ff164ac92c7181f14520
Girish Ranganathan <girish.rranganathan@gmail.com>
2019-11-08 20:17:02 -0500

    adding more logs
    Signed-off-by: Girish Ranganathan <girish.rranganathan@gmail.com>

--- api/gowrk2.go
+++ api/gowrk2.go
@@ -84,10 +84,12 @@ RETRY:
 	if err := json.Unmarshal(out, &raw); err != nil {
 		retryCount++
 		if retryCount <= 1 {
-			logrus.Debug("there was an unmarshal error, retrying with a tiny change")
+			logrus.Debugf("starting with bytes: %s", out)
 			in := string(out)
 			ind := strings.Index(in, "\\n")
+			logrus.Debugf("index of the first new line: %d", ind)
 			if ind > -1 && ind+1 < len(in) {
+				logrus.Debug("there was an unmarshal error, retrying with a tiny change")
 				in = in[ind+1:]
 				out = []byte(in)
 				logrus.Debugf("new output: %s", out)
