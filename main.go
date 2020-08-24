5a194e4843c195095a2b4e47b19940f66c15dde6
Kush Trivedi <kushthedude@gmail.com>
2020-08-24 23:34:55 +0530

    fix
    Signed-off-by: Kush Trivedi <kushthedude@gmail.com>

--- main.go
+++ main.go
@@ -11,11 +11,12 @@ func main() {
 	logrus.SetLevel(logrus.DebugLevel)
 	config := &api.GoWRK2Config{
 		Thread:            1,
-		DurationInSeconds: 5,
-		RQPS:        10,
-		URL:         "http://gmail.com",
-		Percentiles: []float64{50, 75, 90, 99, 99.99, 99.999},
-		Labels:      "",
+		DurationInSeconds: 30,
+		// Connection:     10,
+		RQPS:              10,
+		URL:               "https://meshery.io/",
+		Percentiles:       []float64{50, 75, 90, 99, 99.99, 99.999},
+		Labels:            "",
 	}
 	result, _ := api.WRKRun(config)
 	logrus.Infof("WRK Result: %+v", result)
